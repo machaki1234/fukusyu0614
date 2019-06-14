c11ccc341b892e3d8887b23b693f31283c6c65c5
retnton <38481077+retnton@users.noreply.github.com>
2019-06-14 13:26:03 +0900

    Initial commit

---
+++ fukusyu0614/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukusyu0614
+{
+    public partial class Form1 : Form
+    {
+        int vx = -1;
+        int vy = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left >=ClientSize.Width)
+            {
+                label1.Left = vx;
+            }
+            
+        }
+    }
+}
